73216fe260c086b77af18409fcaa5de14e085387
Mindi Richia Putri <80597699+mindiputri30@users.noreply.github.com>
2023-05-16 20:38:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 # import all the app dependencies
 import pandas as pd
 import numpy as np
-# import sklearn
+import sklearn
 import streamlit as st
 import joblib
-# import matplotlib
+import matplotlib
 from IPython import get_ipython
 from PIL import Image
 
